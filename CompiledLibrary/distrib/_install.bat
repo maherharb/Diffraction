75064368f804ebb6d834483400aa06ff3d57ba65
mharb75 <maher.harb@gmail.com>
2016-05-27 13:03:44 -0400

    removed the compiled dll

--- CompiledLibrary/distrib/_install.bat
+++
@@ -1,9 +0,0 @@
-echo off
-echo Deploying project DIFFWIZ
-IF EXIST MCRInstaller.exe (
-echo Running MCRInstaller
-MCRInstaller.exe
-)
-
-echo Installation complete.
-echo Please refer to the documentation for any additional setup steps.
